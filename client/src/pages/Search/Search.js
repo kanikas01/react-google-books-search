ec268f3e2400796dbd8217eef690b49a554aa55c
kanikas01 <larvakurt@yahoo.com>
2019-04-30 12:16:04 -0700

    Add skeleton handleFormSubmit function

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
 import "./Search.css";
 
+
 class Search extends Component {
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(event.target);
+  };
+
   render () {
     return (
       <div>
@@ -16,7 +23,11 @@ class Search extends Component {
               <h5 className="card-title">Book Search</h5>
               <form className="form-inline my-2 my-lg-0">
                 <input className="form-control mr-sm-2 mb-4 mt-2 col-12" id="search-input" type="search" placeholder="Title" aria-label="Search" />
-                <button className="btn btn-secondary my-sm-0 col-sm-12 col-md-4 col-lg-2" type="submit">Search</button>
+                <button 
+                  className="btn btn-secondary my-sm-0 col-sm-12 col-md-4 col-lg-2" 
+                  type="submit"
+                  onClick={this.handleFormSubmit}
+                >Search</button>
               </form>
             </div>
           </div>
