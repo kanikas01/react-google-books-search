8d1dae968ce0cc605e8688e1fb3ed185396a5006
kanikas01 <larvakurt@yahoo.com>
2019-04-30 12:15:08 -0700

    Add additional route and remove unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import { Col, Row, Container } from "./components/Grid";
-import Jumbotron from "./components/Jumbotron";
-import Results from "./components/Results";
 import SavedBooks from "./pages/SavedBooks";
 import Search from "./pages/Search";
 import NoMatch from "./pages/NoMatch";
@@ -23,6 +21,7 @@ class App extends Component {
           <Nav location={this.state.location}/>
           <Switch>
             <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
             <Route exact path="/saved" component={SavedBooks} />
             <Route component={NoMatch} />
           </Switch>
